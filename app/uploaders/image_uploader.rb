41c58e25d168ece654243dc0f668f52c0c8ec7a0
jcgdxn@hotmail.com <jcgdxn@hotmail.com>
2019-12-10 23:32:32 +0000

    L22+23, Liking where things seem to work now

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,9 +11,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"    #AWS specific
-    uploader = ImageUploader.new
-    uploader.store!(my_file)
-    uploader.retrieve_from_store!('my-file.png')
+  
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
