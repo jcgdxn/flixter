d35cf5fd68720f3116571ae5a16c6417d0e266f4
jcgdxn@hotmail.com <jcgdxn@hotmail.com>
2020-01-09 05:22:35 +0000

    L29_finit_L30_init_placeholder

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,11 @@
 #to permit use of publishable & secret API keys to interact with Stripe
 
+
   Rails.configuration.stripe = {
     :publishable_key => ENV['PUBLISHABLE_KEY'],
     :secret_key      => ENV['SECRET_KEY']
     }
 
-
+    def stripeapi
       Stripe.api.key = Rails.configuration.stripe[:secret_key]
+    end
