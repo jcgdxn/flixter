c0103378cf672de507f9d1f852c6ae4849c0adbd
jcgdxn@hotmail.com <jcgdxn@hotmail.com>
2020-01-15 05:10:43 +0000

    L31_got to the end & entered code specified_ Lookin for typos

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -2,7 +2,8 @@
 
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_section
+  before_action :require_authorized_for_current_section, only: [:new, :create]
+  before_action :require_authorized_for_current_section, only: [:update]
 
       def new
         @lesson = Lesson.new
@@ -13,7 +14,22 @@ class Instructor::LessonsController < ApplicationController
         redirect_to instructor_course_path(current_section.course)
       end
 
+      def update
+        current_lesson.update_attributes(lesson_params)
+        render plain: 'Lesson_Row order updated !!'
+      end
+
 private
+
+      def require_authorized_for_current_lesson
+        if current_lesson.section.course.user !=current_user
+          render plain: 'Unauthorized', status: :unauthorized
+        end
+      end
+
+      def current_lesson
+        @current_lesson ||= Lesson.find(params[:id])
+      end
 
       def require_authorized_for_current_section
         if current_section.course.user != current_user
@@ -27,6 +43,6 @@ private
       end
 
       def lesson_params
-        params.require(:lesson).permit(:title, :subtitle, :video)
+        params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
       end
 end
